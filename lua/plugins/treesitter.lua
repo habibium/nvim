8af20d1a679f5b66dfe7e47c716449c59015bbe3
habibium <habib.prodev@gmail.com>
2025-01-02 23:27:40 +0600

    refactor: Remove LSP configuration and clean up lazy-lock.json

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -7,7 +7,6 @@ local M = {
         local configs = require("nvim-treesitter.configs")
   
         configs.setup({
-            ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "typescript", "html" },
             sync_install = false,
             highlight = { enable = true },
             indent = { enable = true },  
