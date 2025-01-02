327ae36a2ee4af2c09085e639bc728efd7ec66e3
habibium <habib.prodev@gmail.com>
2025-01-02 23:13:37 +0600

    feat: Add mason and telescope

--- init.lua
+++ init.lua
@@ -13,25 +13,15 @@ vim.g.mapleader = ' '
 
 -- Normal mode mappings
 vim.keymap.set('n', '<leader>e', ':Vex<CR>')
-vim.keymap.set('n', '<C-p>', ':GFiles<CR>')
-vim.keymap.set('n', '<leader>ff', ':Files<CR>')
 vim.keymap.set('n', '<C-j>', ':cnext<CR>')
 vim.keymap.set('n', '<C-k>', ':cprev<CR>')
 vim.keymap.set('n', '<leader>y', '"+y')
 vim.keymap.set('n', '<leader>Y', 'ggVG"+y')
 vim.keymap.set('n', '<leader>d', '"_d')
-vim.keymap.set('n', '∆', ':m .+1<CR>==')
-vim.keymap.set('n', '˚', ':m .-2<CR>==')
 
 -- Visual mode mappings
 vim.keymap.set('v', '<leader>p', '"_dP')
 vim.keymap.set('v', '<leader>y', '"+y')
 vim.keymap.set('v', '<leader>d', '"_d')
-vim.keymap.set('v', '∆', ":m '>+1<CR>gv=gv")
-vim.keymap.set('v', '˚', ":m '<-2<CR>gv=gv")
-
--- Insert mode mappings
-vim.keymap.set('i', '∆', '<Esc>:m .+1<CR>==gi')
-vim.keymap.set('i', '˚', '<Esc>:m .-2<CR>==gi')
 
 require("config.lazy")
---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,70 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      "williamboman/mason.nvim",
+      "williamboman/mason-lspconfig.nvim",
+      "hrsh7th/nvim-cmp",
+      "hrsh7th/cmp-nvim-lsp",
+    },
+    config = function()
+      local mason = require("mason")
+      local mason_lspconfig = require("mason-lspconfig")
+      local lspconfig = require("lspconfig")
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+      mason.setup()
+      mason_lspconfig.setup({
+        ensure_installed = { "lua_ls", "elixirls" },
+        automatic_installation = true,
+      })
+
+      -- LSP server configurations
+      lspconfig.lua_ls.setup({ capabilities = capabilities })
+      lspconfig.elixirls.setup({ capabilities = capabilities })
+
+      -- Key mappings
+      vim.keymap.set('n', 'gd', vim.lsp.buf.definition)
+      vim.keymap.set('n', 'K', vim.lsp.buf.hover)
+      vim.keymap.set('n', '<F2>', vim.lsp.buf.rename)
+    end
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      "L3MON4D3/LuaSnip",
+      "saadparwaiz1/cmp_luasnip",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+    },
+    config = function()
+      local cmp = require("cmp")
+      local luasnip = require("luasnip")
+
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            luasnip.lsp_expand(args.body)
+          end,
+        },
+        mapping = cmp.mapping.preset.insert({
+          ['<C-Space>'] = cmp.mapping.complete(),
+          ['<CR>'] = cmp.mapping.confirm({ select = true }),
+          ['<Tab>'] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_next_item()
+            else
+              fallback()
+            end
+          end),
+        }),
+        sources = {
+          { name = 'nvim_lsp' },
+          { name = 'luasnip' },
+          { name = 'buffer' },
+          { name = 'path' },
+        },
+      })
+    end
+  }
+}
